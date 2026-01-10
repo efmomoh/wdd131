2d7b9797cd83b03f75f72419c621aec272998163
efmomoh <efmomoh25@gmail.com>
2026-01-10 15:52:34 +0800

    Modifies the JS file dynamically

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,3 @@
+// footer 
 document.getElementById("lastModified").innerHTML = document.lastModified;
+document.getElementById("currentyear").textContent = `©️ ${new Date().getFullYear()} 🌲 Enssah Fayia Momoh 🌲 Liberia 🏳️❤️🤍💙`
