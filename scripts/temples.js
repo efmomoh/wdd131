ae8e141d886182aad9cd0b10e04212d2ed0ae915
efmomoh <efmomoh25@gmail.com>
2026-01-20 08:07:51 +0800

    Adds JavaScript temple file

---
+++ scripts/temples.js
@@ -0,0 +1,22 @@
+// temple js file
+
+// footer content 
+const currentYear = document.getElementById("currentyear");
+const lastModified = document.getElementById("lastModified");
+
+// modify element 
+const year = new Date().getFullYear();
+currentYear.textContent = `${year} 🕰️ Enssah Fayia Momoh🌲Liberia`;
+const modified = document.lastModified;
+lastModified.textContent = `Last Modified: ${modified}`;
+
+
+// menu / hamburgur 
+const menu = document.querySelector("#menu");
+const nav = document.querySelector("nav ul");
+
+// add event listener 
+menu.addEventListener("click", () => {
+    nav.classList.toggle("open");
+    menu.classList.toggle("open");
+});
